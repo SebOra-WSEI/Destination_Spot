4e4a9e65cae6f2c8493cc0931e3cb7116efc1e26
SebOra-WSEI <LiBroPWSZ@gmail.com>
2024-05-04 13:12:31 +0200

    Move files

---
+++ backend/auth/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	 "fmt"
+	 "github.com/SebOra-WSEI/auth/start"
+	 "github.com/gin-gonic/gin"
+	 "github.com/joho/godotenv"
+	 "log"
+	 "net/http"
+)
+
+const Port = ":8081"
+
+func main() {
+	 if err := godotenv.Load(); err != nil {
+		  fmt.Println(err.Error())
+		  log.Fatal("Error loading .env file")
+	 }
+	
+	 start.Database()
+
+	 r := gin.Default()
+
+	 r.GET(
+		  "/", func(c *gin.Context) {
+				c.JSON(
+					 http.StatusOK, gin.H{
+						  "Service": "Auth",
+					 },
+				)
+		  },
+	 )
+
+	 log.Fatal(r.Run(Port))
+}
