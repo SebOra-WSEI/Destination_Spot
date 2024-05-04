cf13a29bfe17b23dadacd6a03d168b5011ad65cc
SebOra-WSEI <LiBroPWSZ@gmail.com>
2024-05-04 12:54:49 +0200

    Connect to db

--- backend/auth/cmd/main.go
+++ backend/auth/cmd/main.go
@@ -1,25 +1,40 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"log"
-	"net/http"
+	 "fmt"
+	 "github.com/gin-gonic/gin"
+	 "github.com/jinzhu/gorm"
+	 _ "github.com/jinzhu/gorm/dialects/mysql"
+	 "log"
+	 "net/http"
 )
 
 const Port = ":8081"
 
+var Db *gorm.DB
+
 func main() {
-	r := gin.Default()
+	 var err error
 
-	r.GET(
-		"/", func(c *gin.Context) {
-			c.JSON(
-				http.StatusOK, gin.H{
-					"Service": "Auth",
-				},
-			)
-		},
-	)
+	 Db, err = gorm.Open("mysql", "user:0JcusWWFmfZyXrq8VomQ@tcp(database:3306)/destination_spot")
+	 if err != nil {
+		  fmt.Println("Error while connecting to the database:", err.Error())
+		  panic("Failed to connect to the database")
+	 }
 
-	log.Fatal(r.Run(Port))
+	 fmt.Println("*** Auth Service successfully connected to the database ***")
+
+	 r := gin.Default()
+
+	 r.GET(
+		  "/", func(c *gin.Context) {
+				c.JSON(
+					 http.StatusOK, gin.H{
+						  "Service": "Auth",
+					 },
+				)
+		  },
+	 )
+
+	 log.Fatal(r.Run(Port))
 }
