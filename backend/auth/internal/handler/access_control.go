0bcf9408f005bfd0e4512855806ab0453f0408c0
SebOra-WSEI <LiBroPWSZ@gmail.com>
2024-05-05 17:07:20 +0200

    remove unnecessary line

--- backend/auth/internal/handler/access_control.go
+++ backend/auth/internal/handler/access_control.go
@@ -1,25 +1,23 @@
 package handler
 
 import (
-	"github.com/SebOra-WSEI/auth/internal/response"
-	"github.com/SebOra-WSEI/auth/internal/token"
-	"github.com/gin-gonic/gin"
-	"net/http"
+	 "github.com/SebOra-WSEI/auth/internal/response"
+	 "github.com/SebOra-WSEI/auth/internal/token"
+	 "github.com/gin-gonic/gin"
+	 "net/http"
 )
 
 const AuthHeader = "Authorization"
 
 func AccessControl(c *gin.Context) {
-	t, err := token.Verify(c.GetHeader(AuthHeader))
-	if err != nil {
-		c.JSON(http.StatusBadRequest, response.CreateErrorResponse(err.Error()))
-		return
-	}
+	 t, err := token.Verify(c.GetHeader(AuthHeader))
+	 if err != nil {
+		  c.JSON(http.StatusBadRequest, response.CreateErrorResponse(err.Error()))
+		  return
+	 }
 
-	if !t.Valid {
-		c.JSON(http.StatusBadRequest, response.CreateErrorResponse(response.TokenExpiredErrMsg))
-		return
-	}
-
-	c.JSON(200, "abc")
+	 if !t.Valid {
+		  c.JSON(http.StatusBadRequest, response.CreateErrorResponse(response.TokenExpiredErrMsg))
+		  return
+	 }
 }
