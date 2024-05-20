241175d3b836e25b522d125cdb9ff2b17a1e7c8b
SebOra-WSEI <LiBroPWSZ@gmail.com>
2024-05-20 15:23:10 +0200

    Add methods

--- backend/auth/internal/model/user.go
+++ backend/auth/internal/model/user.go
@@ -1,5 +1,11 @@
 package model
 
+import (
+	"fmt"
+	"github.com/SebastianOraczek/auth/database"
+	"github.com/SebastianOraczek/auth/internal/response"
+)
+
 type User struct {
 	Id       uint
 	Email    string
@@ -7,6 +13,43 @@ type User struct {
 	Role     string
 	Name     string
 	Surname  string
+}
+
+func (u User) FindByEmail(email string, user *User) error {
+	if err := database.Db.Where("email = ?", email).First(&user).Error; err != nil {
+		fmt.Println("User not found:", err.Error())
+		return response.ErrInvalidLoginOrPassword
+	}
+
+	return nil
+}
+
+func (u User) FindById(id string, user *User) error {
+	if err := database.Db.First(&user, id).Error; err != nil {
+		fmt.Println("User not found:", err.Error())
+		return response.ErrUserNotFound
+	}
+
+	return nil
+}
+
+func (u User) Update(user *User) error {
+	if err := database.Db.Save(&user).Error; err != nil {
+		fmt.Println("Problem saving user", err.Error())
+		return response.ErrWhileUpdatingUser
+	}
+
+	return nil
+}
+
+func (u User) GetWithNoPassword() NoPasswordUser {
+	return NoPasswordUser{
+		ID:      u.Id,
+		Email:   u.Email,
+		Name:    u.Name,
+		Surname: u.Surname,
+		Role:    u.Role,
+	}
 }
 
 type NoPasswordUser struct {
