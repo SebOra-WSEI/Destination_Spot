4ecd31f90a5b28034b3359fb484fda37148dcc6c
SebOra-WSEI <LiBroPWSZ@gmail.com>
2024-11-18 12:09:11 +0100

    Add corse policy

--- backend/services/core/internal/route/route.go
+++ backend/services/core/internal/route/route.go
@@ -9,6 +9,8 @@ import (
 )
 
 func Init(r *gin.Engine) {
+	r.Use(getCors())
+
 	// Auth service endpoints
 	r.POST("/sign-in", auth.SignIn)
 	r.POST("/sign-up", auth.SignUp)
@@ -32,4 +34,23 @@ func Init(r *gin.Engine) {
 	r.GET("/reservations/:id", reservation.GetById)
 	r.DELETE("/reservations/:id", reservation.Delete)
 	r.PUT("/reservations/:id", reservation.Update)
+}
+
+func getCors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set(
+			"Access-Control-Allow-Headers",
+			"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With",
+		)
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
