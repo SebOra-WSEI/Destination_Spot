84911b40583084c99000508a4bfaeb4961d7de4f
SebOra-WSEI <LiBroPWSZ@gmail.com>
2025-02-04 17:11:19 +0100

    Improve some spots api

--- backend/services/core/internal/model/spot.go
+++ backend/services/core/internal/model/spot.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"github.com/SebOra-WSEI/Destination_spot/shared/response"
 	"github.com/jinzhu/gorm"
@@ -33,16 +35,78 @@ func (u Spot) FindById(db *gorm.DB, id string, spot *Spot) error {
 	return nil
 }
 
-func (u Spot) FindByLocation(db *gorm.DB, location int, spot *Spot) error {
-	if err := db.Where("location = ?", location).First(&spot).Error; err == nil {
+func (u Spot) FindByIdSQL(db *sql.DB, c context.Context, id string, spot *Spot) error {
+	rows, err := db.QueryContext(c, "SELECT * FROM spots WHERE id = ?", id)
+	if err != nil {
+		fmt.Println("Spot not found:", err.Error())
+		return response.ErrSpotNotFound
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		if err := rows.Scan(&spot.ID, &spot.Location); err != nil {
+			fmt.Println("Problem with scanning:", err.Error())
+			return response.ErrInternalServer
+		}
+	}
+
+	if spot.ID == 0 {
+		return response.ErrSpotNotFound
+	}
+
+	return nil
+}
+
+func (u Spot) FindAllSQL(db *sql.DB, c context.Context, spots *[]Spot) error {
+	rows, err := db.QueryContext(c, "SELECT * FROM spots")
+	if err != nil {
+		fmt.Println("Spots not found:", err.Error())
+		return response.ErrUserNotFound
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var spot Spot
+
+		if err := rows.Scan(&spot.ID, &spot.Location); err != nil {
+			fmt.Println("Problem with scanning:", err.Error())
+			return response.ErrInternalServer
+		}
+
+		*spots = append(*spots, spot)
+	}
+
+	if len(*spots) == 0 {
+		return response.ErrUsersNotFound
+	}
+
+	return nil
+}
+
+func (u Spot) FindByLocationSQL(db *sql.DB, c context.Context, location int, spot *Spot) error {
+	rows, err := db.QueryContext(c, "SELECT * FROM spots WHERE location = ?", location)
+	if err != nil {
+		fmt.Println("Spot not found:", err.Error())
+		return response.ErrSpotNotFound
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		if err := rows.Scan(&spot.ID, &spot.Location); err != nil {
+			fmt.Println("Problem with scanning:", err.Error())
+			return response.ErrInternalServer
+		}
+	}
+
+	if spot.ID != 0 {
 		return response.ErrSpotAlreadyExists
 	}
 
 	return nil
 }
 
-func (u Spot) Create(db *gorm.DB, newSpot *Spot) error {
-	if err := db.Create(&newSpot).Error; err != nil {
+func (u Spot) CreateSQL(db *sql.DB, c context.Context, newSpot *Spot) error {
+	if _, err := db.QueryContext(c, "INSERT INTO spots (location) VALUE (?)", newSpot.Location); err != nil {
 		fmt.Println("Problem while creating a new spot", err.Error())
 		return response.ErrProblemWhileCreatingNewSpot
 	}
